e8923f1bd0380213b18a798bf815f7ae0764ce2f
elmar alibeyli <elmar.amanov.2015@mail.ru>
2025-01-18 15:41:05 +0400

    observable lessons

---
+++ App-11-state_managment_rxjs/HomePageApp/src/components/HomeContent/HomeContent.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import QuickBooking from "../QuickBooking/QuickBooking.jsx";
+import "./HomeContent.scss";
+import RoutingContext from "../../utils/RoutingProvider.js";
+
+const MovieCard = React.lazy(() => import("components/MovieCard"));
+
+const dummyItem = [{ name: "Dummy Movie" }];
+
+const HomeContent = (props) => {
+  const [movies, setMovies] = useState(dummyItem);
+
+  useEffect(async () => {
+    // Add the logic to load the movies from server and set to the state
+    async function fetchMovies() {
+      const response = await fetch("http://localhost:5555/movies");
+      const data = await response.json();
+      setMovies(data);
+      console.log(data);
+    }
+    fetchMovies();
+  }, []);
+
+  const movieClicked = (item) => {
+    if (typeof props.movieClicked === "function") {
+      props.movieClicked(item);
+    }
+  };
+
+  const renderMovieList = () => {
+    return movies.map((item) => {
+      return (
+        <div onClick={() => movieClicked(item)} key={item.name}>
+          <MovieCard title={item.name} imageUrl={item.imageUrl} />
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="home-content-container">
+      <RoutingContext.Provider value={props.routing}>
+        <QuickBooking />
+
+        <div className="movies-container">
+          <React.Suspense fallback={<div>Loading...</div>}>
+            {renderMovieList()}
+          </React.Suspense>
+        </div>
+      </RoutingContext.Provider>
+    </div>
+  );
+};
+
+export default HomeContent;
---
+++ App-11-state_managment_rxjs/HomePageApp/src/index.js
@@ -0,0 +1,3 @@
+import("./bootstrap").then((module) => {
+  console.log("Bootstrap yükləndi:", module);
+});
